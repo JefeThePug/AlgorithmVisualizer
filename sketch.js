4c781ea6c3b1920cd9ffd3c634686dace97d4968
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2024-12-07 18:37:41 +0900

    wonky gitignore

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const DarkBG = "#0B192C"
+const LightBG = ""
+
+function setup() {
+    createCanvas(1200, 600); // inside <main>
+    imageMode(CENTER);
+    textAlign(CENTER, CENTER);
+}
+
+function draw() {
+    background(DarkBG);
+}
